94aaf3908d233fe7c489f90b2d1188136680e06e
Zhiwei Zhang <zhiwei2017@gmail.com>
2024-03-17 00:41:38 +0100

    Use joblib for login cookies storing and loading.

--- crawlinsta/login.py
+++ crawlinsta/login.py
@@ -1,5 +1,5 @@
 import logging
-import pickle
+import joblib
 import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -17,7 +17,7 @@ logger = logging.getLogger("crawlinsta")
 def login(driver: Union[Chrome, Edge, Firefox, Safari, Remote],
           username: str,
           password: str,
-          cookies_path: str = "instagram_cookies.pkl") -> bool:
+          cookies_path: str = "instagram_cookies.joblib") -> bool:
     """Login to instagram with given credential, and return True if success,
     else False.
 
@@ -48,6 +48,7 @@ def login(driver: Union[Chrome, Edge, Firefox, Safari, Remote],
     try:
         decline_optional_cookies_btn = driver.find_element(By.XPATH, '//button[@tabindex="0"][text()="Decline optional cookies"]')
         decline_optional_cookies_btn.click()
+        time.sleep(3)
     except NoSuchElementException:
         logger.error("Optional cookies popup not found")
 
@@ -65,7 +66,7 @@ def login(driver: Union[Chrome, Edge, Firefox, Safari, Remote],
     time.sleep(10)
 
     # Store cookies in a file
-    pickle.dump(driver.get_cookies(), open(cookies_path, "wb"))
+    joblib.dump(driver.get_cookies(), cookies_path)
 
     try:
         not_turn_on_notifications_btn = driver.find_element(By.XPATH, '//button[@tabindex="0"][text()="Not Now"]')
@@ -83,13 +84,13 @@ def login_with_cookies(driver: Union[Chrome, Edge, Firefox, Safari, Remote],
         driver (selenium.webdriver.remote.webdriver.WebDriver): selenium
          driver for controlling the browser to perform certain actions.
         cookies_path (str): The path to the file containing cookies.
-            Default is "instagram_cookies.pkl".
+            Default is "instagram_cookies.joblib".
 
     Examples:
         >>> from crawlinsta import webdriver
         >>> driver = webdriver.Chrome('path_to_chromedriver')
         >>> from crawlinsta.login import login_with_cookies
-        >>> login_with_cookies(driver, 'path_to_cookies_file.pkl')
+        >>> login_with_cookies(driver, 'path_to_cookies_file.joblib')
         >>> time.sleep(5)
         >>> driver.quit()
     """
@@ -101,7 +102,7 @@ def login_with_cookies(driver: Union[Chrome, Edge, Firefox, Safari, Remote],
     except NoSuchElementException:
         logger.error("Optional cookies popup not found")
 
-    cookies = pickle.load(open(cookies_path, "rb"))
+    cookies = joblib.load(cookies_path)
     for cookie in cookies:
         driver.add_cookie(cookie)
     driver.refresh()

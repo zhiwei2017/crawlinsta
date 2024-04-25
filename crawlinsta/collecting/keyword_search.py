7224c12bb56704d5078abe4e81ff98f3505495b7
Zhiwei Zhang <zhiwei2017@gmail.com>
2024-04-25 22:44:25 +0200

    Fix flake8 error.

--- crawlinsta/collecting/keyword_search.py
+++ crawlinsta/collecting/keyword_search.py
@@ -111,9 +111,13 @@ class SearchWithKeyword(CollectBase):
             del self.driver.requests
             not_pers_btn = self.driver.find_element(
                 By.XPATH,
-                '//div[@aria-label="Not personalised" or @aria-label="Not personalized" or @aria-label="Nicht personalisiert"]'
+                '//div[@aria-label="Not personalised" '
+                'or @aria-label="Not personalized" '
+                'or @aria-label="Nicht personalisiert"]'
                 '[@role="button"][@tabindex="0"]'
-                '//span[text()="Not personalised" or text()="Not personalized" or text()="Nicht personalisiert"]')
+                '//span[text()="Not personalised" '
+                'or text()="Not personalized" '
+                'or text()="Nicht personalisiert"]')
             not_pers_btn.click()
             time.sleep(random.SystemRandom().randint(6, 8))
 
